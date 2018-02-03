30978c2601ee76ad5dda012210e3df9a1ccb3566
Tywunda Shivers <tywundashivers@TiMac.fios-router.home>
2018-02-03 13:34:04 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,30 @@
-// Create variables to store each cards
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+// Create an array to store all cards
+var cards = ['queen', 'queen', 'king', 'king'];
 
-// Use console.log to display the cards user flipped
+// Create an array to store cards in play
+var cardsInPlay = [];
+
+// Create a variable cardOne for the first card the user flipped
+var cardOne = cards[0];
+// Add card to array for cardsInPlay
+cardsInPlay.push(cardOne);
+// Use console.log to display the card the user flipped
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+// Create a variable cardTwo for the second card the user flipped
+var cardTwo = cards[2];
+// Add card to array for cardsInPlay
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+// Check to see if two cards have been played
+if (cardsInPlay.length === 2) {
+
+  // Check to see if two cards match and alert users
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
