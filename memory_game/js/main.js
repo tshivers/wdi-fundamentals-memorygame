1b9e57582d0debb41f457d7982a54813047807e4
Tywunda Shivers <tywundashivers@TiMac.fios-router.home>
2018-02-04 19:33:45 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,42 +22,35 @@ var cards = [
 }
 ];
 
-// Create an array to store cards in play
 var cardsInPlay = [];
 
-//Create a function to check for a match
 var checkForMatch = function () {
-  // Check to see if two cards match and alert user
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
-}
-};
-
-// Create a function to show the user flipped a card
-var flipCard = function (cardId) {
-
-// Display the card the user flipped over in console.log
-console.log("User flipped " + cards[cardId].rank);
-
-// Add card to array of cards in play
-cardsInPlay.push(cards[cardId].rank);
-
-// Display image for card flipped in console.log
-console.log(cards[cardId].cardImage)
-
-// Display suit for card flipped in console.log
-console.log(cards[cardId].suit)
-
-  // Check to see if two cards have been played
-  if (cardsInPlay.length === 2) {
-  // If so, call checkForMatch function
-  checkForMatch();
   }
 };
 
-// Call flipCard function passing in 0 as the argument
-flipCard(0);
-// Call flipCardfunction passing in 2 as the argument
-flipCard(2);
+var flipCard = function () {
+  cardId = this.getAttribute('data-id');
+  console.log(cardId);
+  cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+    cardsInPlay = [];
+  }
+};
+
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+};
+
+createBoard();
